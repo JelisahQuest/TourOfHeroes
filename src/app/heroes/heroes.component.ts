5c9869fa3af2f42d77bfc07b2876389a0600e324
Alex Nelson <alexander.e.nelson@gmail.com>
2018-06-01 03:01:47 -0400

    Added heroes list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { FormsModule } from '@angular/forms';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,10 +10,12 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelectHero(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   constructor() { }
 
